2472ca8953d80ce5a714dbac5efce1844fa77473
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-08-19 00:42:25 -0600

    clear button functionlity added

--- main.js
+++ main.js
@@ -18,7 +18,23 @@ calculator.addEventListener("click", function (event) {
   if (event.target.classList.contains("number")) {
     updateDisplay(event);
   }
-  if (event.target.classList.contains("operator")) {
-    performOperation();
+  //   if (event.target.classList.contains("operator")) {
+  //     performOperation();
+  //   }
+  if (event.target.id === "clear") {
+    // console.log("Clear Button Clicked");
+    displayValue = "0";
+    history = undefined;
+    stringArr = [];
+    resultDisplay.innerText = displayValue;
+  }
+  if (event.target.id === "backspace" || event.target.id === "backsp") {
+    console.log("BS Clicked");
+    let displayLength = displayValue.length;
+    displayValue = displayValue.slice(0, displayLength - 1);
+    if (displayValue === "") {
+      displayValue = "0";
+    }
+    resultDisplay.innerText = displayValue;
   }
 });
